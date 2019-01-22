3aa028de94a091ba1182111152be7538e77040a3
OussamaAzeddine <45151124+OussamaAzeddine@users.noreply.github.com>
2019-01-22 14:51:21 +0100

    Update BasicStatisticsWarmup.py

--- AI and Machine learning CHallenges/BasicStatisticsWarmup.py
+++ AI and Machine learning CHallenges/BasicStatisticsWarmup.py
@@ -1,10 +1,13 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-#Ongoing ....
+from math import sqrt
+import numpy as np
+
 def func_median(N,tab):
+  tab = np.sort(tab)
   if(N%2 == 1):
     return tab[N//2]
   else:
-    return (tab[int(N/2)]+tab[int(N/2)-1])/2
+    return round((tab[N//2]+tab[(N//2)-1])/2,1)
 
 def func_mean(N,tab):
   count = 0
@@ -13,15 +16,33 @@ def func_mean(N,tab):
   return round(count/N,1)
 
 def func_mode(N,tab):
-  higher_occ_val = 0
-  occ = 0
+  #get max occurences
+  occ =0
+  #find number of max occurences
   for k in range(N):
-    occ = tab.count(tab[k])
-    if( occ > higher_occ_val): 
-      higher_occ_val = occ
-  return higher_occ_val
+    if(occ<tab.count(tab[k])):
+      occ = tab.count(tab[k])
+  #find the minimum with max_occurences
+  mode = max(tab)
+  for k in range(N):
+    if(occ == tab.count(tab[k])):
+      if(tab[k]< mode): mode = tab[k]
+  return mode
 
-"{{f_list_all_subject.display}}"
+def fun_sd(N,tab):
+  square_sum = 0
+  mean = func_mean(N,tab)
+  for k in range(N):
+    square_sum += (tab[k]-mean)**2
+  return round(sqrt(square_sum/N),1)
+
+def fun_cdint(N,tab):
+  mean = func_mean(N,tab)
+  stand = fun_sd(N,tab)
+  upper_bound = round(mean + (stand*1.96/sqrt(N)),1) # the formula is mean +/- (standard_deviation*z-value/sqrt(numberofSamples)) where Z-value is the equivalent of 95% for confidence interval
+  lower_bound = round(mean - (stand*1.96/sqrt(N)),1)
+  return str(lower_bound)+' '+str(upper_bound)
+
 
 if __name__ == '__main__':
   #read Data
@@ -30,3 +51,5 @@ if __name__ == '__main__':
   print(func_mean(N,tab_entry))
   print(func_median(N,tab_entry))
   print(func_mode(N,tab_entry))
+  print(fun_sd(N,tab_entry))
+  print(fun_cdint(N,tab_entry))
